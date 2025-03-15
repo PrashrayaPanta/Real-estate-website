86a288e84229db561bbf769cd7ef186da96f441b
Prashraya Panta <prashrayapanta33@gmail.com>
2025-03-15 13:23:55 +0545

    Remove the inline js and used external js

---
+++ Carousels.js
@@ -0,0 +1,24 @@
+let index = 0;
+const slides = document.querySelectorAll(".slide");
+const totalSlides = slides.length;
+
+function showSlide(i) {
+  index = (i + totalSlides) % totalSlides; // Handle wrap-around
+  document.querySelector(".slider").style.transform = `translateX(-${
+    index * 100
+  }%)`;
+}
+
+function nextSlide() {
+  showSlide(index + 1);
+}
+
+function prevSlide() {
+  showSlide(index - 1);
+}
+
+document.querySelector(".prev").onclick = prevSlide;
+
+document.querySelector(".next").onclick = nextSlide;
+
+export { index, slides, totalSlides, showSlide, nextSlide, prevSlide };
---
+++ main.js
@@ -0,0 +1,14 @@
+import { HousesArrayData } from "./Houses.js";
+
+console.log(HousesArrayData);
+
+import {
+  index,
+  slides,
+  totalSlides,
+  showSlide,
+  nextSlide,
+  prevSlide,
+} from "./Carousels.js";
+
+console.log(nextSlide, prevSlide, slides, index, totalSlides, showSlide);

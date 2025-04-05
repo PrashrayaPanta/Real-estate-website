0d8b053779ac699d81281a3ff9c8dd4c6c925037
Prashraya Panta <prashrayapanta33@gmail.com>
2025-04-05 22:23:17 +0545

    Add Functionality Done

--- addHouse.js
+++ addHouse.js
@@ -1,142 +1,88 @@
-
-
 const houseNameElement = document.querySelector("#title");
 
-
-// console.log(houseNameElement);
-
-
+console.log(houseNameElement);
 
 const noofRoomElement = document.querySelector("#noofroom");
 
-// console.log(noofRoomElement);
+console.log(noofRoomElement);
 
 const squareFeetCoveredElement = document.querySelector("#squarefeetcovered");
 
-// console.log(squareFeetCoveredElement);
-
+console.log(squareFeetCoveredElement);
 
 const locatedAtElement = document.querySelector("#located-at");
 
-// console.log(locatedAtElement);
-
-const titleValue = houseNameElement.value;
-
-
-const noofRoomElementValue = noofRoomElement.value;
-
-
-const squareFeetCoveredElementValue = squareFeetCoveredElement.value;
-
-
-const locatedAtElementValue = locatedAtElement.value;
-
-
-const inputAllement = document.querySelectorAll(".form-input");
-
-
-console.log(inputAllement);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(locatedAtElement);
 
 const addTodoElement = document.querySelector(".submit-btn");
 
+const houses = JSON.parse(localStorage.getItem("houses")) || [];
 
-addTodoElement.addEventListener("click", function(event){
+console.log(houses);
 
+houses.map((todo) => {
+  const houseListElement = document.createElement("div");
 
+  houseListElement.classList.add("house-list");
 
+  houseListElement.innerHTML = `<h2>House Title:${todo.titleValue}</h2>
+          <h2>House No of Room: ${todo.noofRoomElementValue}</h2>
+          <h2>square Foot: ${todo.squareFeetCoveredElementValue}</h2>
+          <h2>Located At :${todo.locatedAtElementValue}</h2>`;
 
+  const containerHouseListsElement = document.querySelector(
+    ".container-house-lists"
+  );
 
+  containerHouseListsElement.appendChild(houseListElement);
+});
 
+addTodoElement.addEventListener("click", function (event) {
+  const titleValue = houseNameElement.value;
 
+  const noofRoomElementValue = noofRoomElement.value;
 
+  const squareFeetCoveredElementValue = squareFeetCoveredElement.value;
 
+  const locatedAtElementValue = locatedAtElement.value;
 
+  console.log(
+    titleValue,
+    noofRoomElementValue,
+    squareFeetCoveredElementValue,
+    locatedAtElementValue
+  );
 
-    
-    
+  houses.push({
+    titleValue,
+    noofRoomElementValue,
+    squareFeetCoveredElementValue,
+    locatedAtElementValue,
+  });
 
+  localStorage.setItem("houses", JSON.stringify(houses));
 
+  console.log(houses);
 
-    console.log(titleValue, noofRoomElementValue, squareFeetCoveredElementValue, locatedAtElementValue);
-    
+  houses.map((todo) => {
+    const houseListElement = document.createElement("div");
+    houseListElement.classList.add("house-list");
 
+    houseListElement.innerHTML = `<h2>House Title:${todo.titleValue}</h2>
+          <h2>House No of Room: ${todo.noofRoomElementValue}</h2>
+          <h2>square Foot: ${todo.squareFeetCoveredElementValue}</h2>
+          <h2>Located At :${todo.locatedAtElementValue}</h2>`;
 
+    const containerHouseListsElement = document.querySelector(
+      ".container-house-lists"
+    );
 
-    const todos = JSON.parse(sessionStorage.getItem("todos")) || [];
-
-
-    console.log(todos);
-
-    todos.map((todo) => {
-
-        // console.log(todo);
-
-        const houseListElement = document.createElement("div");
-
-        houseListElement.classList.add("house-list");
-
-        houseListElement.innerHTML = `<h2>House Title:${todo.titleValue}</h2>
-        <h2>House No of Room: ${todo.noofRoomElementValue}</h2>
-        <h2>square Foot: ${todo.squareFeetCoveredElementValue}</h2>
-        <h2>Located At :${todo.locatedAtElementValue}</h2>`;  
-
-
-
-
-    })
-    
-
-    todos.push({titleValue, noofRoomElementValue, squareFeetCoveredElementValue, locatedAtElementValue});
-
-
-    sessionStorage.setItem("todos", JSON.stringify(todos));
-
-
-    console.log(todos);
-    
-
-
-
-
-    
-
-
-
-
-
-
-}) 
+    containerHouseListsElement.appendChild(houseListElement);
+  });
 
 
-// console.log(addTodoElement);
-
-
-// addTodoElement.onclick
+    houseNameElement.value = "";
+    noofRoomElement.value = "";
+    squareFeetCoveredElement.value = "";
+    locatedAtElement.value = "";
+});
